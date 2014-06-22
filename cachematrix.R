71fbc3ebe27e8c4f3b7bf1ae1e9eb05f208cce9c
DF102 <featherstone.daniel@gmail.com>
2014-06-22 16:50:14 +0100

    Thrid Attempt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to calculate the invers of a matrix
+## If the inverse of a matrix has already been calculated the 
+## result will be retrieved from a cache to avoid recalculating
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a list of 4 functions ($set, $get, $setinverse, 
+## $getinverse) A matrix can be input into this list using $set.
+## This list can then be passed to cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a makeCacheMatrix object and calculates 
+## the inverse of the matrix present. If the inverse has not previously
+## been calculated it is stored in the cache. If it has it is retrieved
+## from the cache and no calculation takes place.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
+
